b677054a0d2fa17a9fa75857e74aff3554ddb3da
abhishekjena222 <abhishekjena222@gmail.com>
2025-08-20 22:50:35 +0530

    add a html spite

---
+++ src/main/java/com/example/libraryManagement/controller/GenericController.java
@@ -0,0 +1,25 @@
+package com.example.libraryManagement.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenericController {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String home() {
+        return """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Welcome</title>
+            </head>
+            <body>
+                <h1>Welcome to My App 🚀</h1>
+                <p>Welcome to My Library.</p>
+            </body>
+            </html>
+            """;
+    }
+}
