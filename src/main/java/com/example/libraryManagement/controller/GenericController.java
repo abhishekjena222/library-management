fb5250cb9da22fe75e58c71cbafecf7017817f84
abhishekjena222 <abhishekjena222@gmail.com>
2025-08-21 12:55:39 +0530

    added a static html page

--- src/main/java/com/example/libraryManagement/controller/GenericController.java
+++ src/main/java/com/example/libraryManagement/controller/GenericController.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenericController {
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    public String home() {
-        return """
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Welcome</title>
-            </head>
-            <body>
-                <h1>Welcome to My App 🚀</h1>
-                <p>Welcome to My Library.</p>
-            </body>
-            </html>
-            """;
-    }
+//    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+//    public String home() {
+//        return """
+//            <!DOCTYPE html>
+//            <html>
+//            <head>
+//                <title>Welcome</title>
+//            </head>
+//            <body>
+//                <h1>Welcome to My App 🚀</h1>
+//                <p>Welcome to My Library.</p>
+//            </body>
+//            </html>
+//            """;
+//    }
 }
